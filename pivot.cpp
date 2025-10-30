939660be5304e5a0ef5ca516d06955617ea16d26
Ankur <tiwariankur2004@gmail.com>
2025-10-30 23:41:40 +0530

    finding pivot index

---
+++ pivot.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int getPivot(int arr[], int n){
+    int s=0,e=n-1;
+    int mid= s+ (e-s)/2;
+    while(s<e){
+        if(arr[mid]>=arr[0]){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+        mid = s+ (e-s)/2;
+    }
+    return s;
+
+}
+int main(){
+    int arr[5]={3,8,13,1,2};
+    cout<<"Pivot index is "<<getPivot(arr,5)<<endl;
+
+}
