9c21c2c31b8bcbf4ca4d99eda10ad327172c847c
Ankur <tiwariankur2004@gmail.com>
2025-11-02 18:46:31 +0530

    Optimizing bubble sort algo

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,14 +3,18 @@ using namespace std;
 int main(){
     int n=10;
     int arr[10]={5,16,0,79,13,168,4,8,109,43};
+    bool swapped;
     for(int i=0;i<n-1;i++){//for rounds
+        swapped=false;
         for(int j=0;j<n-i-1;j++){//for swap
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
-            }
+                swapped = true; 
 
+             }
+    
         }
-
+        if(!swapped) break;
     }
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
